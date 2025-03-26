2e8baadf49a87948bca30328ecee2ba0963fe44d
omkar070402 <omkarseshadri007@gmail.com>
2025-03-26 19:29:34 +0530

    commit

--- my-react-app/src/Components/Navbar.jsx
+++ my-react-app/src/Components/Navbar.jsx
@@ -22,14 +22,14 @@ const Navbar = () => {
         < div >
             <nav className="navbar navbar-expand-lg bg-dark text-white p-2 shadow" id='navbar'>
                 <div className="container-fluid p-2">
-                    <NavLink className="navbar-brand text-white" to='/'>TO-Do-App</NavLink>
+                    <NavLink className="navbar-brand text-white ms-3" to='/'>TO-Do-App</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent ">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav m-auto mb-2 mb-lg-0 " >
-                            <li className="nav-item">
-                                <NavLink className="nav-link text-white" aria-current="page" to={'/'}>Home</   NavLink>
+                            <li className="nav-item ms-5">
+                                <NavLink className="nav-link text-white" aria-current="page" to={'/'}>Home</NavLink>
                             </li>
                             <li className="nav-item ms-5">
                                 <NavLink className="nav-link text-white" to={'/mytask'}>My Task</NavLink>
@@ -41,7 +41,10 @@ const Navbar = () => {
                                         <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
                                     </>
                                 ) : (
-                                    <NavLink className="nav-link text-white" to="/login">Login</NavLink>
+                                    <NavLink to="/login" className="nav-link text-white">
+                                        Login
+                                    </NavLink>
+
                                 )}
 
                             </li>
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import { ToastContainer, toast } from 'react-toastify';
 import { TaskProvider } from './Context/TaskContext.jsx'
 
