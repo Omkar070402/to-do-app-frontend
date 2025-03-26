7530422f5edc50581657ab7901db53a60817f8a0
omkar070402 <omkarseshadri007@gmail.com>
2025-03-26 21:45:49 +0530

    commit

--- my-react-app/src/Pages/Login.jsx
+++ my-react-app/src/Pages/Login.jsx
@@ -19,7 +19,6 @@ const Login = () => {
       e.preventDefault()
 
       const response = await axios.post(`${API_BASE_URL}/api/v1/user/login`, data)
-      alert(response.data.message)
       console.log(response.data);
       toast.success(response.data.message)
       localStorage.setItem('token', response.data.token)
@@ -36,30 +35,42 @@ const Login = () => {
 
 
   return (
-    <div className="d-flex align-items-center justify-content-center vh-100" style={{ fontFamily: "monospace" }}>
-      <div className="w-50 shadow-lg p-4 rounded">
-        <h3 className="text-center mb-3">Login</h3>
 
-        <form className='d-flex flex-column gap-3' onSubmit={(e) => handleSubmit(e)} >
-          <div>
-            <label>Email:</label>
-            <input type="email" placeholder='Enter email' name='email' value={data.email} onChange={(e) => handleData(e)} className='form-control' />
-          </div>
 
-          <div>
-            <label>Password:</label>
-            <input type="password" placeholder='Enter password' name='password' value={data.password} onChange={(e) => handleData(e)} className='form-control' />
-          </div>
 
-          <div className='d-flex justify-content-between'>
-            <p className="text-primary">Forgot Password?</p>
-            <p className="text-primary">Don't have an account? <NavLink to={'/signup'} className='text-decoration-none'>Signup</NavLink></p>
-          </div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="container">
 
-          <div className='text-center'>
-            <button className='btn btn-dark px-4 fw-bold'>Login</button>
+            <div className="d-flex align-items-center justify-content-center vh-100" style={{ fontFamily: "monospace" }}>
+              <div className="w-50 shadow-lg p-4 rounded">
+                <h3 className="text-center mb-3">Login</h3>
+
+                <form className='d-flex flex-column gap-3' onSubmit={(e) => handleSubmit(e)} >
+                  <div>
+                    <label>Email:</label>
+                    <input type="email" placeholder='Enter email' name='email' value={data.email} onChange={(e) => handleData(e)} className='form-control' />
+                  </div>
+
+                  <div>
+                    <label>Password:</label>
+                    <input type="password" placeholder='Enter password' name='password' value={data.password} onChange={(e) => handleData(e)} className='form-control' />
+                  </div>
+                  <div className='d-flex justify-content-between'>
+                    <p className="text-primary">Forgot Password?</p>
+                    <p className="text-primary">Don't have an account? <NavLink to={'/signup'} className='text-decoration-none'>Signup</NavLink></p>
+                  </div>
+
+                  <div className='text-center'>
+                    <button className='btn btn-dark px-4 fw-bold'>Login</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
